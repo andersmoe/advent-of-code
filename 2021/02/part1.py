4858afa0878cbc478092eb80ef13b2cd55b3cfa4
Anders G. Moe <andersm@netcompany.com>
2022-01-18 10:15:59 +0100

    Cleanup solution for day 2 part 1

--- 2021/02/part1.py
+++ 2021/02/part1.py
@@ -3,14 +3,13 @@ commands = [line.split(" ") for line in lines]
 
 horizontal = 0
 depth = 0
+
 for command in commands:
     if command[0] == 'forward':
-        horizontal = horizontal + int(command[1])
+        horizontal += int(command[1])
     elif command[0] == 'up':
-        depth = depth - int(command[1])
+        depth -= int(command[1])
     elif command[0] == 'down':
-        depth = depth + int(command[1])
+        depth += int(command[1])
 
-print("Horizontal: " + str(horizontal))
-print("Depth: " + str(depth))
 print("Answer: " + str(horizontal * depth))
